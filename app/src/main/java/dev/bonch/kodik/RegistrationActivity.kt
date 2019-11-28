d3c7eefcaed66a01018d9152a8d7d7ae8416d885
Phenecy <phenecyow@gmail.com>
2019-11-28 23:52:36 +0300

    backup commit

--- app/src/main/java/dev/bonch/kodik/RegistrationActivity.kt
+++ app/src/main/java/dev/bonch/kodik/RegistrationActivity.kt
@@ -2,12 +2,39 @@ package dev.bonch.kodik
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import kotlinx.android.synthetic.main.activity_registration.*
 
 class RegistrationActivity : AppCompatActivity() {
+
+    private lateinit var userNameEditText: EditText
+    private lateinit var emailEditText: EditText
+    private lateinit var passwordEditText: EditText
+    private lateinit var passwordRepeatEditText: EditText
+    private lateinit var registrationButton: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_registration)
 
+        initializeViews()
+        clickListeners()
+
+
+    }
+
+    private fun initializeViews() {
+        userNameEditText = registration_name_edit_text
+        emailEditText = registration_email_edit_text
+        passwordEditText = registration_password_edit_text
+        passwordRepeatEditText = registration_password_repeat_edit_text
+        registrationButton = registration_button
+    }
+
+    private fun clickListeners(){
+        registrationButton.setOnClickListener {
+            TODO("Добавить переход на главную активность")
+        }
     }
 }
